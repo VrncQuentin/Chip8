ecf8e71af1d27a244d9f140d6b96de6ccb234c96
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-07-05 06:36:38 +0200

    update(repo): improved architecture update(vm): improved architecture feat(vm): added basic screen implementation (data structure + dump in term)

---
+++ src/c8.hpp
@@ -0,0 +1,20 @@
+#ifndef C8_HPP
+#define C8_HPP
+
+/* LibC & STL */
+#include <cstdint>
+
+/* Project */
+
+namespace Chip8 {
+
+    using byte = uint8_t;
+    using opcode = uint16_t;
+
+    struct Specs {
+        static constexpr uint16_t memory_size = 4096;
+        static constexpr uint16_t load_addr = 512;
+    };
+}
+
+#endif /* C8_HPP */
---
+++ src/c8vm/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Screen.hpp"
+
+int main()
+{
+    Chip8::VM::Screen scr;
+
+    scr.dumpScreen();
+}
