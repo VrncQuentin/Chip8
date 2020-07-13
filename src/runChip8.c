440d3522ba580750f03fb0844d211ad834611f86
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-07-13 17:40:44 +0200

    update(run): improved loop to reflect fetch -> decode -> execute cycle, altho there's some further decoding in certain executions

--- src/runChip8.c
+++ src/runChip8.c
@@ -1,24 +1,33 @@
 #include "interpreter.h"
 #include "instructions.h"
 
-USE_MAJORS_TAB;
 
-static uint16_t getNextInstr(const uint8_t *prog, const uint16_t pc)
+static uint16_t fetch(const uint8_t *prog, const uint16_t pc)
 {
-    return prog[pc] << 8 | prog[pc + 1];;
+    return prog[pc] << 8 | prog[pc + 1];
+}
+
+static opfn decode(const uint16_t op)
+{
+    uint16_t major = MAJOR_IDX(op);
+    USE_MAJORS_TAB;
+
+    if (major >= MAJORS)
+        continue;
+    return MAJORS_TAB[major];
 }
 
 int runChip8(struct Chip8 *c)
 {
     uint8_t *prog = c->memory.beginProg;
     uint16_t op;
+    opfn execute;
 
-    while ((op = getNextInstr(prog, c->pc))) {
+    while ((op = fetch(prog, c->pc))) {
         c->pc += 2;
-        uint16_t major = MAJOR_IDX(op);
-        if (major >= MAJORS)
+        if (!(execute = decode()))
             continue;
-        MAJORS_TAB[major](c, op);
+        execute(c, op);
     };
     return 0;
 }
